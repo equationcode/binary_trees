e1d1a1b8e8ac014da0bbfa3e60b1d4c0c10ad762
equationcode <annypaul80@gmail.com>
2023-09-04 01:34:26 -0700

    112-array_to_bst.c

---
+++ 112-array_to_bst.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - function that builds a Binary Search Tree from an
+ * array
+ * @array: the array to turns to BST tree
+ * @size: the size of array
+ * Return: a pointer to the root node of the created BST, or NULL
+ * on failure
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i = 0;
+	bst_t *root;
+
+	root = NULL;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (; i < size; i++)
+	{
+		if (i == 0)
+		{
+			bst_insert(&root, array[i]);
+		}
+		else
+		{
+			bst_insert(&root, array[i]);
+		}
+	}
+	return (root);
+}
